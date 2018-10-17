c2135b62eec28470fe4ab1d6e0e0b18fb09527ac
James Schultz <james.schultz@A1-10110010.local>
2018-10-17 11:34:31 -0600

    provided .json example to go along with serve data drill 👇🏿

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const people = require('./people')
 const port = 3000
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res, next) => {
+  res.json({people})
+})
 
 app.listen(port, () => {
   console.log(`I got you on ${port}`)
---
+++ people.js
@@ -0,0 +1,22 @@
+module.exports = [{
+  firstName: 'james',
+  lastName: 'smells ',
+  age: 1000,
+  favColor: 'orange'
+}, {
+  firstName: 'Patrick',
+  lastName: 'Star',
+  age: 2,
+  favColor: 'pink'
+}, {
+  firstName: 'Peter',
+  lastName: 'Ostiguy',
+  age: 25,
+  favColor: 'Florence and The Machine Gray'
+
+}, {
+  firstName: 'James',
+  lastName: 'Mann',
+  age: 22,
+  favColor: 'black'
+}]
